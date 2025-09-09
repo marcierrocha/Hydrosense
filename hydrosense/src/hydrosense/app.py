c6236e1444e318a84721fd1f1d07c0c91875504f
marcierrocha <rochamarcier59@gmail.com>
2025-09-09 12:45:36 +0100

    Mudando o app para webview

--- hydrosense/src/hydrosense/app.py
+++ hydrosense/src/hydrosense/app.py
@@ -12,42 +12,25 @@ class HydroSense(toga.App):
 
         main_box = toga.Box(
 
-                style=Pack(
-                background_color="#FF0000",
-                direction=COLUMN,
-            )
+
         )
 
+        webview = toga.WebView(style= Pack(flex=1))
+
+        main_box.add(webview)
+        webview.url = "http://192.168.0.90:8080"
 
 
-        name_label = toga.Label(
-            "Your name: ",
-            margin=(0, 5),
-            direction=ROW
-        )
-        self.name_input = toga.TextInput(flex=1)
 
-        name_box = toga.Box(direction=ROW, margin=5, )
-        name_box.add(name_label)
-        name_box.add(self.name_input)
 
-        button = toga.Button(
-            "Say Hello!",
-            on_press=self.say_hello,
-            margin=5,
-            background_color='blue',
-        )
 
-        # CORRIGIDO: estas linhas agora estão bem indentadas
-        main_box.add(name_box)
-        main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
+
         self.main_window.content = main_box
         self.main_window.show()
 
-    def say_hello(self, widget):
-        print(f"Hello, {self.name_input.value}")
+
 
 
 def main():
